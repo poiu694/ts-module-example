5995cef2efbbc1c6a40e66e5be4d97ac87024501
Lee <43488305+poiu694@users.noreply.github.com>
2022-10-18 11:08:24 +0900

    fix: beforeEach 없애기
    각 테스트마다 사용되는 변수가 제각각

--- __test__/util.test.ts
+++ __test__/util.test.ts
@@ -13,35 +13,32 @@ describe('getObjectDeepKey', () => {
   let simple1: Simple1, simple2: Simple2;
   let complex1: Complex1, complex2: Complex2, complex3: Complex3;
 
-  beforeEach(() => {
-    simple1 = { a: 1, b: 2 }; // Object
-    simple2 = [1, 2, 3]; // Array
-    complex1 = { a: 1, b: 2, c: [1, 2, 3] };
-    complex2 = [1, 2, { a: 1, b: 2 }];
-    complex3 = [1, 2, { a: 1, b: 2, d: [1, 2, { a: 1, b: 1 }] }];
-  });
-
   it('simple case#1, 단순 depth1 Object', () => {
+    simple1 = { a: 1, b: 2 }; // Object
     const key = getObjectDeepKeys(simple1);
     expect(key).toStrictEqual(['a', 'b']);
   });
 
   it('simple case#2, 단순 depth1 Array', () => {
+    simple2 = [1, 2, 3]; // Array
     const key = getObjectDeepKeys(simple2);
     expect(key).toStrictEqual(['0', '1', '2']);
   });
 
   it('complex case#1, Object 내부 Array', () => {
+    complex1 = { a: 1, b: 2, c: [1, 2, 3] };
     const key = getObjectDeepKeys(complex1);
     expect(key).toStrictEqual(['a', 'b', 'c', 'c[0]', 'c[1]', 'c[2]']);
   });
 
   it('complex case#2, Array 내부 Object', () => {
+    complex2 = [1, 2, { a: 1, b: 2 }];
     const key = getObjectDeepKeys(complex2);
     expect(key).toStrictEqual(['0', '1', '2', '2.a', '2.b']);
   });
 
   it('complex case#3, mixed case', () => {
+    complex3 = [1, 2, { a: 1, b: 2, d: [1, 2, { a: 1, b: 1 }] }];
     const key = getObjectDeepKeys(complex3);
     expect(key).toStrictEqual([
       '0',
